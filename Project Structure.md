2256d0439770fc9eda35dee67bf34552f41b0a91
saka <869621380@qq.com>
2025-07-27 17:17:37 +0800

    add Project Structure.md

---
+++ Project Structure.md
@@ -0,0 +1,9 @@
+# Project Structure
+
+## 实现简单的Bean容器
+
+![bean1.png](assets/bean1.png)
+
+## Bean实例化
+
+![bean2.png](assets/bean2.png)
--- src/main/java/org/springframework/bean/factory/support/CglibSubclassingInstantiationStrategy.java
+++ src/main/java/org/springframework/bean/factory/support/CglibSubclassingInstantiationStrategy.java
@@ -3,11 +3,9 @@ package org.springframework.bean.factory.support;
 import org.springframework.bean.factory.BeansException;
 import org.springframework.bean.factory.config.BeanDefinition;
 
-public class CglibSubclassingInstantiationStrategy implements  InstantiationStrategy {
+public class CglibSubclassingInstantiationStrategy extends SimpleInstantiationStrategy {
 
-    @Override
-    public Object instantiate(BeanDefinition beanDefinition) throws BeansException {
-        //TODO 使用Cglib动态生成子类
-        throw new UnsupportedOperationException("CGLIB instantiation strategy is not supported now");
-    }
+
+    //TODO 使用Cglib动态生成子类
+
 }
